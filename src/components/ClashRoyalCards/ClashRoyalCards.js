435bc9480edfe3f8cb0320dbcb476ec958f5287f
pz1996r <pz1996r@gmail.com>
2020-04-25 19:19:45 +0200

    adding styles to components

--- src/components/ClashRoyalCards/ClashRoyalCards.js
+++ src/components/ClashRoyalCards/ClashRoyalCards.js
@@ -1,29 +1,84 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import FadeLoader from "react-spinners/FadeLoader";
 
+const Stripe = css`
+    display: flex;
+    width: 100%;
+    height:28px;
+    background:white;
+`;
 const Loader = styled.div`
+    transform: translate(-50%, -50%);
     position: absolute;
     display: block;
     margin: 0 auto;
     top:50%;
     left:50%;
     >div{
+        transform: translate(26px, 20px);
         top:0px;
         left:0px;
     }
 `;
 const ImageLoaderWrapper = styled.div`
+    background:#ffffff14;
     position: relative;
     width: 277px;
     height:330px;
-
-
 `;
 
 const IMG = styled.img`
     display: ${({ visible }) => { return (visible ? 'block' : 'none') }};
+`;
+
+const Label = styled.div`
+    ${Stripe}
+    color:black;
+    justify-content:center;
+    align-items:center;
+`;
+
+const StepperMenager = styled.div`
+    ${Stripe}
+`;
+
+const Status = styled.div`
+    flex-grow:2;
+    display:flex;
+    align-items:center;
+    position: relative;
+
+    ::before{
+        content:'';
+        display:block;
+        border-radius:4px;
+        background: #c4c4c4;
+        width:96%;
+        height:4px;
+    }
+    ::after{
+        content:' ';
+        position: absolute;
+        justify-self: start;
+        display:block;
+        height:4px;
+        width:90%;
+        border-radius: 4px;
+        background:#4141d3;
+    }
+
+`;
+
+const Button = styled.button`
+    padding: 0 20px;
+    border:none;
+    height:100%;
+    cursor: pointer;
+    :focus{
+        outline:none;
+    }
 `;
 
 export default () => {
@@ -65,6 +120,7 @@ export default () => {
 
     return (
         <div>
+            <Label>{cardName}</Label>
             <ImageLoaderWrapper>
                 <IMG alt="card" src={image} visible={status === 'LOADED'} onLoad={() => { console.log(99); setStatus('LOADED') }}></IMG>
                 <Loader>
@@ -75,11 +131,11 @@ export default () => {
                     />
                 </Loader>
             </ImageLoaderWrapper>
-            <br />
-            <button onClick={() => { changeCard(imageId - 1) }}>prev</button>
-            <label>{cardName}</label>
-            <label>{`${imageId}/${cardsAmount}`}</label>
-            <button onClick={() => { changeCard(imageId + 1) }}>next</button>
+            <StepperMenager>
+                <Button onClick={() => { changeCard(imageId - 1) }}>BACK</Button>
+                <Status />
+                <Button onClick={() => { changeCard(imageId + 1) }}>NEXT</Button>
+            </StepperMenager>
         </div>
     )
 }
