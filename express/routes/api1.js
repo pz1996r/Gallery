2888c052e64ac0472542af33ea18f71a40c35916
pz1996r <pz1996r@gmail.com>
2020-04-30 18:04:36 +0200

    checking server ip

--- express/routes/api1.js
+++ express/routes/api1.js
@@ -1,7 +1,14 @@
 const api = require('../routes.js')
 const router = api.router;
 
+const ip = require("ip");
+const serverIp = ip.address();
+const Authorization = process.env.Authorization;
+
 router.get('/api1', async (req, res) => {
+    console.log('api', serverIp);
+    console.log('env', Authorization);
+    console.log()
     res.json({
         'hello': 'api'
     })
